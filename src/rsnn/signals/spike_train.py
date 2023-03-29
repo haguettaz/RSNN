22c079145c82d0d9d45fd75e56d17498a15e80be
haguettaz <hugaguett@gmail.com>
2023-03-29 16:46:26 +0200

    convolution with fft and ifft in spike train sampling

--- src/rsnn/signals/spike_train.py
+++ src/rsnn/signals/spike_train.py
@@ -1,8 +1,6 @@
 from typing import Optional, Union
 
 import numpy as np
-from scipy import signal
-from tqdm.autonotebook import tqdm
 
 from .utils import norm
 
@@ -19,31 +17,29 @@ class SpikeTrain:
 
         self.firing_times = [np.array([]) for _ in range(num_channels)]
 
-    def __repr__(self):
-        string = f"SpikeTrain(num_channels={self.num_channels}, duration={self.duration})"
-        return string
-    
-    def generate(self, res=1e-2, rtol=1e-3):
+    def random(self, res=1e-2, rmax=1):
+        # 0. initialize
+        self.firing_times = []
+
         if self.soft_refractory_period > 0:
             hazard = lambda t_ : (t_ >= self.hard_refractory_period) * self.firing_rate * (1 - np.exp(-(t_ - self.hard_refractory_period) / self.soft_refractory_period )) # hazard function = conditional firing rate
-            Hazard = lambda t_ : (t_ >= self.hard_refractory_period) * self.firing_rate * self.soft_refractory_period * ((t_ - self.hard_refractory_period) / self.soft_refractory_period + np.exp(-(t_ - self.hard_refractory_period) / soft_refractory_period) - 1)
+            Hazard = lambda t_ : (t_ >= self.hard_refractory_period) * self.firing_rate * self.soft_refractory_period * ((t_ - self.hard_refractory_period) / self.soft_refractory_period + np.exp(-(t_ - self.hard_refractory_period) / self.soft_refractory_period) - 1)
         else:
             hazard = lambda t_ : (t_ >= self.hard_refractory_period) * self.firing_rate
             Hazard = lambda t_ : (t_ >= self.hard_refractory_period) * self.firing_rate * t_
 
-        pdf = lambda t_ : hazard(t_) * np.exp(-Hazard(t_)) # pdf
+        q = lambda t_ : hazard(t_) * np.exp(-Hazard(t_)) # pdf
 
-        num_samples = int(self.duration // res)
-        times = np.linspace(0, self.duration, num_samples)
+        # assume a spike occurs at time 0, create a spike train of duration 2*duration
+        t = np.arange(0, rmax*self.duration, res)
+        qt = norm(q(t))
 
-        q = norm(pdf(times))
-
-        self.firing_times = []
         for _ in range(self.num_channels):  # for each channel
-            sn = np.array([0.0])
-            while sn[-1] < self.duration:
-                sn = np.append(sn, sn[-1] + self.rng.choice(times, p=q))
-            self.firing_times.append(sn[1:-1])
+            # uniformly chose the window's origin s0 in between -rmax*duration and 0
+            s = self.rng.uniform(-rmax*self.duration, 0, 1)
+            while s[-1] < self.duration:
+                s = np.append(s, s[-1] + self.rng.choice(t, p=qt))
+            self.firing_times.append(s[(s>=0) & (s<self.duration)])
 
 class PeriodicSpikeTrain:
     def __init__(self, num_channels, period, firing_rate, hard_refractory_period, soft_refractory_period, rng=None):
@@ -56,12 +52,10 @@ class PeriodicSpikeTrain:
         self.soft_refractory_period = soft_refractory_period
 
         self.firing_times = [np.array([]) for _ in range(num_channels)]
-
-    def __repr__(self):
-        string = f"PeriodicSpikeTrain(num_channels={self.num_channels}, period={self.period})"
-        return string
     
-    def generate(self, res=1e-2, rtol=1e-3):
+    def random(self, res=1e-2, rtol=1e-4, rmax=5):
+        # 0. initialize
+        self.firing_times = []
 
         if self.soft_refractory_period > 0:
             hazard = lambda t_ : (t_ >= self.hard_refractory_period) * self.firing_rate * (1 - np.exp(-(t_ - self.hard_refractory_period) / self.soft_refractory_period )) # hazard function = conditional firing rate
@@ -70,53 +64,44 @@ class PeriodicSpikeTrain:
             hazard = lambda t_ : (t_ >= self.hard_refractory_period) * self.firing_rate
             Hazard = lambda t_ : (t_ >= self.hard_refractory_period) * self.firing_rate * t_
 
-        pdf = lambda t_ : hazard(t_) * np.exp(-Hazard(t_)) # pdf
-        survival = lambda t_ : np.exp(- Hazard(t_)) # survival
-        no_survival = lambda t_ : 1 - np.exp(- Hazard(t_))
+        q = lambda t_ : hazard(t_) * np.exp(-Hazard(t_))
+        Q = lambda t_ : 1 - np.exp(- Hazard(t_))
 
-        num_samples = int(2*self.period // res)
-        times = np.linspace(0, 2*self.period, num_samples)
+        # implicit zero padding with rmax
+        t = (np.arange(0, self.period, res)[None,:] + self.period * np.arange(rmax)[:,None]).flatten()
+        qt = norm(q(t))
+        idx = np.ceil(self.period/res).astype(int) # index in t corresponding to period
 
-        q = pdf(times)
+        # 1. compute the forward messages 
+        qf = np.fft.rfft(qt)
+        tmp = np.copy(qf)
+        msgf = [qt]
+        while np.argmax(msgf[-1]) < idx or msgf[-1][idx] > rtol * np.max(msgf[-1]):
+            tmp = tmp * qf
+            msgf.append(norm(np.around(np.fft.irfft(tmp), 9)))
+        msgf = np.stack(msgf, axis=0)
 
-        # 1. compute forward messages
-        msgf = []
-        msgf.append(signal.unit_impulse(num_samples)) # dirac delta
-        msgf.append(norm(q * times))
-
-        max_p = msgf[-1][num_samples // 2]
-        while True:
-            msgf.append(norm(np.convolve(q, msgf[-1])[:num_samples]))
-            if msgf[-1][num_samples // 2] > max_p:
-                max_p = msgf[-1][num_samples // 2]
-                continue
-            if msgf[-1][num_samples // 2] < rtol * max_p:
-                break
+        # 2. compute the distribution of the number of firings
+        pn = norm(msgf[:, idx])
         
-        msgf = np.stack(msgf)
-        n_max = msgf.shape[0]
-
-        # compute marginal distribution of firing numbers
-        pn = np.empty(n_max)
-        pn[0] = survival(self.period) # no firing
-        pn[1:] = norm(msgf[1:,num_samples // 2]) * no_survival(self.period) # one or more firings
-            
-        self.firing_times = []
+        # 3. sample per channels
         for _ in range(self.num_channels):
-            # 1. sample the number of firings
-            n = self.rng.choice(n_max, p=pn)
-            if n < 1:
+            # 3.a. is there any spike?
+            if self.rng.binomial(1, Q(self.period)) == 0:
                 self.firing_times.append(np.array([]))
                 continue
-
-            # 2. sample the interspike times
-            sn = np.empty(n+1)
-            sn[-1] = self.period
-            for m in range(n-1, -1, -1):
-                psm = norm(msgf[m] * pdf(sn[m+1] - times))
-                sn[m] = self.rng.choice(times, p=psm)
-
-            # 3. sample the exact positions of firing times
-            xn = np.diff(sn)
-            xn[0] = self.rng.uniform(0.0, xn[0])
-            self.firing_times.append(np.cumsum(xn))
+                
+            # 3.b. given there is at least one spike, sample the exact number of spikes in the sequence
+            n = self.rng.choice(len(pn), p=pn) + 1
+            
+            # 3.c. given the exact number of spikes, sample sn, ..., s1 by backward sampling, starting from sn = self.period
+            s = np.empty(n)
+            s[-1] = self.period
+            for m in range(n-2, -1, -1):
+                psm = norm(msgf[m] * q(s[m+1] - t))
+                s[m] = self.rng.choice(t, p=psm)
+                
+            # 3.d independently, sample the origin of the sequence s0, and cyclically shift the sequence
+            s0 = self.rng.uniform(0, self.period)
+            s += s0
+            self.firing_times.append(np.sort(s % self.period))
