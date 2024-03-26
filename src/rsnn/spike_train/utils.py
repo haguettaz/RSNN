16f0e281839c3aae48ac5387a463aff764074d66
haguettaz <hugaguett@gmail.com>
2024-03-26 09:05:48 +0100

    clean spike train's notebook

--- src/rsnn/spike_train/utils.py
+++ src/rsnn/spike_train/utils.py
@@ -19,3 +19,17 @@ def pmf_num_spikes(period:float, firing_rate:float) -> Tuple[np.ndarray, np.ndar
     logpns[1:] -= np.cumsum(np.log(ns[1:]))
     pns = np.exp(logpns)
     return ns, pns / np.sum(pns)
+
+def expected_num_spikes(period:float, firing_rate:float) -> float:
+    """
+    Returns the expected number of spikes in a periodic spike train with a given period and firing rate.
+
+    Args:
+        period (float): the period of the spike train in [tau_min].
+        firing_rate (float): the firing rate of the spike train in [1/tau_min].
+
+    Returns:
+        float: the expected number of spikes.
+    """
+    ns, pns = pmf_num_spikes(period, firing_rate)
+    return np.inner(ns, pns)
